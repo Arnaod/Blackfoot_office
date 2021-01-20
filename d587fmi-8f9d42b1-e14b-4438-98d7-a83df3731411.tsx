509174f8e7fb87a7827cb3b6cc1b107660f22729
Arnaod <lederman.k@gmail.com>
2021-01-20 19:19:53 +0100

    Add files via upload

---
+++ d587fmi-8f9d42b1-e14b-4438-98d7-a83df3731411.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="d587fmi-8f9d42b1-e14b-4438-98d7-a83df3731411" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="d587fmi-8f9d42b1-e14b-4438-98d7-a83df3731411.png" width="256" height="256"/>
+</tileset>
